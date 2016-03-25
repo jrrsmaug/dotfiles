6f9ceaa7795cd1ad2e4a9f559df7fcd62ed3078e
Bernhard Sillipp <bernhard.sillipp@novum-ccservices.at>
2016-03-25 13:18:30 +0100

    added emacs.bat

---
+++ win/emacs.bat
@@ -0,0 +1,2 @@
+SET PATH=C:\msys64\usr\local\bin;C:\msys64\usr\local\sbin;C:\msys64\mingw64\bin;C:\msys64\usr\bin;C:\Users\besillipp\.gem\ruby\2.1.0\bin;C:\msys64\usr\bin\site_perl;C:\msys64\usr\bin\vendor_perl;C:\msys64\usr\bin\core_perl;C:\Users\besillipp\emacs\bin;%PATH%
+start C:\Users\besillipp\emacs\bin\runemacs.exe
